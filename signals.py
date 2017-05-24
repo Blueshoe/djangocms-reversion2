6847e05ad5632e9939a4e43d311f891e5ffe39df
Daniel Pollithy <daniel.pollithy@gmail.com>
2017-05-24 16:42:36 +0200

    Add signal

--- signals.py
+++ signals.py
@@ -32,6 +32,7 @@ def handle_placeholder_change(**kwargs):
 
 
 def handle_page_delete(sender, instance, **kwargs):
+    # deleting a real page will delete all of its hidden versions
     page = instance
 
     for pv in page.page_versions.iterator():
@@ -39,6 +40,15 @@ def handle_page_delete(sender, instance, **kwargs):
         pv.delete()
 
 
+def delete_hidden_page(sender, **kwargs):
+    # deleting a PageVersion deletes its hidden page in the PageTree
+    # This signal handler deletes the hidden page associated to a PageVersion
+    # (reverse to on_delete=models.CASCADE)
+    # Problem was that an infinite loop can be originated
+    # if kwargs['instance'] and kwargs['instance'].hidden_page:
+    #    kwargs['instance'].hidden_page.delete()
+    # TODO: delete the hidden_page of the PageVersion
+    pass
 
 
 def connect_all_plugins():
@@ -46,4 +56,6 @@ def connect_all_plugins():
     post_placeholder_operation.connect(handle_placeholder_change, dispatch_uid='reversion2_placeholder')
     signals.post_save.connect(mark_title_dirty, sender='cms.Title', dispatch_uid='reversion2_title')
     signals.pre_delete.connect(handle_page_delete, sender='cms.Page', dispatch_uid='reversion2_page')
+    signals.post_delete.connect(delete_hidden_page, sender='djangocms_reversion2.PageVersion',
+                                dispatch_uid='reversion2_page_version')
 
